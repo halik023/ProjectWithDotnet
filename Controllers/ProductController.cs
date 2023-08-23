28cc58c772aab449373488c7b874e28b032f4506
Liem Dang <dhliem@tma.com.vn>
2023-08-23 18:09:33 +0700

    commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using project_with_dotnet.Models;
+
+namespace project_with_dotnet.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController: ControllerBase {
+        public List<Product> products = new List<Product>();
+
+        [HttpGet]
+        public IActionResult GetAll () {
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductVM productVM) {
+            Product product = new Product() {
+                ProductId = Guid.NewGuid(),
+                Name = productVM.Name,
+                Price = productVM.Price
+            };
+            products.Add(product);
+            return Ok(product);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace project_with_dotnet.Models {
+    public class ProductVM {
+        public string Name { set; get;}
+        public double Price {set; get; }
+
+    }
+
+    public class Product : ProductVM {
+        public Guid ProductId { set; get; }
+        
+
+    }
+}
